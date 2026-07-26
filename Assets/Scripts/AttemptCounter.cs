ba66d138cf7158ac194cd0419152760082499364
MuhammadAayanMirza <mmaayanmirza@gmail.com>
2026-07-26 13:23:38 +0500

    So Many New Changes, Read Description!!
    Created A Short Level To Test The Speed Of The Player And Added Game Manager Which Currently Tells The Console When Game Is Over. Added Fog To The Environment So That It Gives A Polished Look To The Game, At The Top It Says How Much Distance You Have Travelled For Now But I Soon Wanna Replace It With No. Of Attempts Or No. Of Deaths.

---
+++ Assets/Scripts/AttemptCounter.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttemptCounter : MonoBehaviour
+{
+   public Transform player;
+   public TextMeshProUGUI scoreText;
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+        
+    }
+}
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovements movement;
+    
     void OnCollisionEnter(Collision collisioninfo)
     {
         if (collisioninfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindAnyObjectByType<GameManager>().EndGame();
         }
     }
 }
