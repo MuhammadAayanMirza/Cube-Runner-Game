dbd357765eff9113c01aa4db5c6222871555e46b
MuhammadAayanMirza <mmaayanmirza@gmail.com>
2026-07-26 09:52:36 +0500

    New Obstacle, Camera Following, Left and Right Controls!!
    I Added The First Obstacle In The Game, Made The Player Body Stop When It Collides With The Obstacle Body, Made The Camera Follow The Player Body, Created A Tag For Obstacles To Reference In The Code Files, And Made A Material For The Obstacle.

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovements movement;
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
